364c4a8cf356cbf1f246437d9308433f01142048
Youssef Kholeif <yykholeif@gmail.com>
2017-12-12 15:55:42 +0000

    debugging

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,6 @@ class UsersController < ApplicationController
   # GET /users/1.json
   def show
     @user = User.find(params[:id])
-    @first_micropost = @user.microposts.first
   end
 
   # GET /users/new
